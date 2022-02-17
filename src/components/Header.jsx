cac5c9dec08bb97f36e6dd912c30e4f1a0395ab6
Supatchaya <tai.pakdee@gmail.com>
2022-02-17 15:02:15 -0700

    adding hero image and text

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div className = "banner">
+            <div className = "banner-content">
+            <div className = "container">
+                <h3>Lorem ipsum dolor sit amet</h3>
+                <h1>Thai Food Recipes</h1>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default header
